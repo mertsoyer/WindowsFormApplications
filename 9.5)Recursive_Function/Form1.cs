25d9754e98834719858b7c592b47e0e03d767476
Mert Soyer <mertsoyer@hotmail.com.tr>
2022-04-07 14:40:54 +0300

    recursive metot

---
+++ 9.5)Recursive_Function/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9._5_Recursive_Function
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private int Faktoriyel_Recursive(int sayi)
+        {
+            if (sayi==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return sayi * Faktoriyel_Recursive(sayi - 1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sonuc = Faktoriyel_Recursive(5);
+            MessageBox.Show(sonuc.ToString());
+        }
+    }
+}
