4e60536e74ca91b3457de13f06840a752029f786
Mert Soyer <mertsoyer@hotmail.com.tr>
2022-04-07 15:01:23 +0300

    IEnumerable

---
+++ 9.6)IEnumarable/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9._6_IEnumarable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string[] isimler = new string[3];
+            //isimler[0] = "SOY39";
+            //isimler[1] = "SOY40";
+            //isimler[2] = "SOY41";
+
+
+            Personel personel = new Personel();
+            foreach (string item in personel)
+            {
+                MessageBox.Show(item);
+            }
+        }
+    }
+}
---
+++ 9.6)IEnumarable/Personel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._6_IEnumarable
+{
+    //IEnumerable , generic olmayan bir koleksiyon üzerinde iterasyon(liste içinde dönmemizi) yapmamızı sağlar.
+    //GetEnumerator metodu bir koleksiyon dizi içinden iterasyon yapmamızı sağlar ve geriye IEnumeratır tipinde bir sınıf döndürür.
+    public class Personel : IEnumerable
+    {
+
+
+
+        string[] personel = new string[3];
+
+        public Personel()
+        {
+            personel[0] = "Mert";
+            personel[1] = "Doğukan";
+            personel[2] = "Soyer";
+        }
+        public IEnumerator GetEnumerator()
+        {
+           return personel.GetEnumerator();
+        }
+    }
+}
